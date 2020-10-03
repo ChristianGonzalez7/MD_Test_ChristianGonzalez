86f824517a0b2cbdbd349156cbadbc53b578f534
ChristianGonzalez7 <christiangonzalez1994@gmail.com>
2020-10-03 10:35:44 +0200

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ShipsComponent } from './ships/ships.component';
+import { StarshipsListComponent } from './ships/starships-list/starships-list.component';
+import { StarshipComponent } from './ships/starships-list/starship/starship.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ShipsComponent,
+    StarshipsListComponent,
+    StarshipComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
